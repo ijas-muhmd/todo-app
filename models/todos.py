10aa359d32838cc14010ad472b40bffb142c7147
ijas <ijasmuhammed20@gmail.com>
2024-02-11 19:46:51 +0530

    updated the models of todo

--- models/todos.py
+++ models/todos.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 class Todo(BaseModel):
     name: str
     description: str
-    completed: bool
+    completed: Optional[bool] = False
     image_path: Optional[str] = None
 
 
--- routes/route.py
+++ routes/route.py
@@ -1,3 +1,4 @@
+
 from datetime import datetime, timezone, timedelta
 from typing import Annotated
 
